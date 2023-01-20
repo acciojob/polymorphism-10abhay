6c5f4f34092d68f29067745ae810586bb48d3c92
10abhay <116686811+10abhay@users.noreply.github.com>
2023-01-21 02:22:31 +0530

    sol3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,10 +7,10 @@ public class Main {
         p.product(2,3,4);
         p.product(2.0,3.0);
     }
+    public static class Product{
+        public int product(int x, int y) {return x*y;}
+        public int product(int x, int y, int z) {return x*y*z;}
+        public double product(double x, double y) {return x*y;}
+    }
 
-}
-class Product{
-    public int product(int x, int y) {return x*y;}
-    public int product(int x, int y, int z) {return x*y*z;}
-    public double product(double x, double y) {return x*y;}
 }
